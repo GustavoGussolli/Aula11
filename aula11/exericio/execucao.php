0cb838ebdac3a136a9ddda62ac4cba42cd859143
GustavoGussolli <134729255+GustavoGussolli@users.noreply.github.com>
2024-12-06 09:58:26 -0300

    Delete aula11/exericio directory

--- aula11/exericio/execucao.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    require_once("modelo/Ingresso.php");
-    require_once("modelo/IngressoVip.php");
-    require_once("modelo/IngressoCamarote.php");
-
-    $ingresso = new Ingresso();
-    $ingressoVip = new IngressoVip();
-    $ingressoCamarote = new IngressoCamarote();
